df382991cb019361e0b0a1bf72ce2ff89bbe4f45
Abhinav Gupta <abhinav23012003@gmail.com>
2026-01-15 20:38:10 +0530

    init commit

---
+++ src/main/java/com/agt/accounts/controller/AccountsController.java
@@ -0,0 +1,4 @@
+package com.agt.accounts.controller;
+
+public class AccountsController {
+}
---
+++ src/test/java/com/agt/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agt.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
