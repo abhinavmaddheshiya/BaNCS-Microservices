d38029cd78224d5a490460cdb63e728e1ec6218c
Abhinav Gupta <abhinav23012003@gmail.com>
2026-01-17 23:19:00 +0530

    DTO Changes

---
+++ src/main/java/com/agt/accounts/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.agt.accounts.repository;
+
+import com.agt.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
